b5e63834dcbe1bdea92a469e303c000445018b18
Jaku Lahtinen <jakulahtinen@msn.com>
2025-05-07 13:17:13 +0300

    Moved the login endpoint to own file and created interface for token.

--- src/components/loginpage.tsx
+++ src/components/loginpage.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import "../styles/login.css";
+import { login } from '../api/authApi';
 
 const LoginPage = () => {
     const [username, setUsername] = useState("");
@@ -13,25 +14,12 @@ const LoginPage = () => {
         e.preventDefault();
         setLoading(true);
         try {
-            const response = await fetch(`${process.env.REACT_APP_API_URL}/authAuth/login`, {
-                method: "POST",
-                headers: { "Content-Type": "application/json" },
-                body: JSON.stringify({ username, password }),
-            });
-    
-            if (response.ok) {
-                const data = await response.json();
-                localStorage.setItem("token", data.token);
-    
-                // Lähetetään storage-event, jotta Navbar huomaa tokenin päivityksen
-                window.dispatchEvent(new Event("storage"));
-    
-                navigate("/admin");
-            } else {
-                setError("Kirjautuminen epäonnistui. Tarkista tiedot.");
-            }
+            const data = await login(username, password);
+            localStorage.setItem("token", data.token);
+            window.dispatchEvent(new Event("storage"));
+            navigate("/admin");
         } catch (err) {
-            setError("Jotain meni pieleen.");
+            setError("Kirjautuminen epäonnistui. Tarkista tiedot.");
         } finally {
             setLoading(false);
         }
