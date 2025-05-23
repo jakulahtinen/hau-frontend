bb9a9b438a407d9cc23115d301691c538d8bf899
Jaku Lahtinen <jakulahtinen@msn.com>
2025-05-23 14:24:40 +0300

    Little refactoring

--- src/components/scorespage.tsx
+++ src/components/scorespage.tsx
@@ -38,49 +38,49 @@ const Scorespage = () => {
 
     return (
             <div className="scores-container">
-            {/* Left-side */}
-            <div className="scores-list">
-                <h2 className="section-title">Tulokset</h2>
-                {scoresList.map((score) => (
-                    <button
-                        key={score.id}
-                        onClick={() => setSelectedScore(score)}
-                        className={`score-title-button ${selectedScore?.id === score.id ? "selected" : ""}`}
-                    >
-                        {score.title}
-                    </button>
-                ))}
-            </div>
+                {/* Left-side */}
+                <div className="scorespage-list">
+                    <h2 className="section-title">Tulokset</h2>
+                    {scoresList.map((score) => (
+                        <button
+                            key={score.id}
+                            onClick={() => setSelectedScore(score)}
+                            className={`score-title-button ${selectedScore?.id === score.id ? "selected" : ""}`}
+                        >
+                            {score.title}
+                        </button>
+                    ))}
+                </div>
 
-            {/* Right-side */}
-            <div className="scores-details">
-                {selectedScore ? (
-                    <div>
-                        <h2 className="score-title">{selectedScore.title}</h2>
-                        <p className="score-date">{formatDate(selectedScore.publishedAt)}</p>
-                            <p className="score-content">
-                                {selectedScore.content.split('\n').map((line, idx) => {
-                                    const trimmed = line.trim();
-                                    const isHeading = /^[A-ZÄÖÅ\s\d]+$/.test(trimmed) && !/^\d/.test(trimmed);
-                                    return (
-                                    <span key={idx}
-                                     style={{
-                                        display: "block",
-                                        fontWeight: isHeading ? "bold" : "normal",
-                                        fontSize: isHeading ? "1.2em" : "1em",
-                                        marginBottom: isHeading ? "10px" : "4px",
-                                        marginTop: isHeading ? "40px" : "0px",
-                                        }}
-                                    >
-                                        {trimmed}
-                                    </span>
-                                    );
-                                })}
-                            </p>
-                    </div>
-                ) : (
-                    <p>Valitse tulos vasemmalta nähdäksesi sisällön.</p>
-                )}
+                {/* Right-side */}
+                <div className="scores-details">
+                    {selectedScore ? (
+                        <div>
+                            <h2 className="score-title">{selectedScore.title}</h2>
+                            <p className="score-date">{formatDate(selectedScore.publishedAt)}</p>
+                                <p className="score-content">
+                                    {selectedScore.content.split('\n').map((line, idx) => {
+                                        const trimmed = line.trim();
+                                        const isHeading = /^[A-ZÄÖÅ\s\d]+$/.test(trimmed) && !/^\d/.test(trimmed);
+                                        return (
+                                        <span key={idx}
+                                        style={{
+                                            display: "block",
+                                            fontWeight: isHeading ? "bold" : "normal",
+                                            fontSize: isHeading ? "1.2em" : "1em",
+                                            marginBottom: isHeading ? "10px" : "4px",
+                                            marginTop: isHeading ? "40px" : "0px",
+                                            }}
+                                        >
+                                            {trimmed}
+                                        </span>
+                                        );
+                                    })}
+                                </p>
+                        </div>
+                    ) : (
+                        <p>Valitse tulos vasemmalta nähdäksesi sisällön.</p>
+                    )}
             </div>
         </div>
     );
